43459750685d237d60ced0a27b660051adf25c4f
saqibhaneef <muhammadsaqib.ms228@gmail.com>
2024-10-04 12:16:55 +0500

    display comments list and writes test

---
+++ testing/src/Components/__tests__/CommentList.test.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { mount } from 'enzyme'
+import Root from 'Root'
+import CommentList from 'Components/CommentList'
+
+
+let wrapped;
+
+beforeEach(() => {
+
+    const initialState = {
+        comments: ['Comment 1', 'Comment 2']
+    }
+
+    wrapped = mount(
+        <Root initialState = {initialState}>
+            <CommentList/>
+        </Root>
+    )
+})
+
+it('creates one LI per comment', () => {
+
+    expect(wrapped.find('li').length).toEqual(2)
+});
+
+it('shows the text for each element', () => {
+    
+    expect(wrapped.render().text()).toContain('Comment 1')
+    expect(wrapped.render().text()).toContain('Comment 2')
+
+});
