5639412ca26699e3e14203c7bd07bfbffe75c35b
saqibhaneef <muhammadsaqib.ms228@gmail.com>
2024-10-25 11:32:37 +0500

    added mongoose model and auth controller

--- prod/auth/server/index.js
+++ prod/auth/server/index.js
@@ -4,13 +4,17 @@ const morgan = require('morgan')
 const http = require('http');
 const app = express();
 const router = require('./router');
+const mongoose = require('mongoose');
 
+//db setup
+mongoose.connect('https://localhost:27017')
 
+//app setup
 app.use(morgan('combined'));
 app.use(bodyParser.json({type: '*/*'}))
 router(app);
 
-
+//server setup
 const port = process.env.PORT || 3090;
 const server = http.createServer(app);
 server.listen(port,()=>{
--- prod/auth/server/router.js
+++ prod/auth/server/router.js
@@ -1,5 +1,8 @@
+const Authentication = require('./controllers/authentication')
+
 module.exports = function(app){
     app.get('/', function(req, res, next){
         res.send(['string1','string2'])
     });
+    app.post('/signup',Authentication.signup);
 }
