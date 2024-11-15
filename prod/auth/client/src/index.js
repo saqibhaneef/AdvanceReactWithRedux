47540542edfbacb679f48a7fc713c531ddc31cd0
saqibhaneef <muhammadsaqib.ms228@gmail.com>
2024-11-15 12:19:55 +0500

    redux form for signup and axios, resolve cors

--- prod/auth/client/src/index.js
+++ prod/auth/client/src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from "react-router-dom";
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk'
 
+import reducers from './reducers';
 import App from './components/App'
 import Welcome from './components/Welcome';
+import Signup from './components/auth/Signup';
 
+const store = createStore(
+  reducers,
+  {},
+  applyMiddleware(reduxThunk)
+)
 
-ReactDOM.render(
+ReactDOM.render(  
+  <Provider store={store}>
   <BrowserRouter>
   <App>
     <Route path="/" exact component={Welcome}></Route>
+    <Route path="/signup" exact component={Signup}></Route>
   </App>
   </BrowserRouter>
+  </Provider>
   ,
   document.querySelector("#root")
 )
